d4498372ee82cb1565d28090ae49cf4207e2a26d
Sean Phillips <seanphillips@protonmail.com>
2019-06-29 12:09:46 +0200

    separated concerns

--- index.js
+++ index.js
@@ -6,27 +6,61 @@ class Block {
     this.timestamp = timestamp;
     this.data = data;
     this.prevHash = prevHash;
-    this.thisHash = sha256(
-      this.index + this.timestamp + this.data + this.prevHash
-    );
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return sha256(this.index + this.timestamp + this.data + this.prevHash);
   }
 }
 
-const createGenesisBlock = () => new Block(0, Date.now(), 'Genesis Block', '0');
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
 
-const nextBlock = (lastBlock, data) =>
-  new Block(lastBlock.index + 1, Date.now(), data, lastBlock.thisHash);
+  createGenesisBlock() {
+    return new Block(0, Date.now(), 'Genesis Block', '0');
+  }
+
+  latestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.prevHash = this.latestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  checkValid() {
+    for(let i = 1; i < this.chain.length; i++){
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if(currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if(previousBlock.hash !== previousBlock.calculateHash()){
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
 
 const createBlockchain = num => {
-  const blockchain = [createGenesisBlock()];
+  const blockchain = new Blockchain();
   let previousBlock = blockchain[0];
 
   for (let i = 1; i < num; i += 1) {
-    const blockToAdd = nextBlock(previousBlock, `This is block #${i}`);
-    blockchain.push(blockToAdd);
-    previousBlock = blockToAdd;
+    const newBlock = blockchain.addBlock(new Block(Date.now(), `this is block ${i}`));
+    previousBlock = newBlock;
   }
   console.log(blockchain);
+  console.log("\n\nIs blockchain valid? " + blockchain.checkValid());
 };
 
 // Generate test data:
